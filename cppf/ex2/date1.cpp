723b73234c7b0fa22ba9b36edba9969a7773bf0d
olvemaudal <oma@pvv.org>
2011-03-21 13:40:18 +0100

    pull out date calculation into a separate headerfile-only library called xyzlib

--- cppf/ex2/date1.cpp
+++ cppf/ex2/date1.cpp
@@ -1,61 +1,26 @@
 #include "date1.hpp"
-
-static bool is_leap_year(int year)
-{
-    return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
-}
-
-static int days_in_month(int year, int month)
-{
-    switch (month) {
-    case 2:
-        return is_leap_year(year) ? 29 : 28;
-    case 4: case 6: case 9: case 11:
-        return 30;
-    }
-    return 31;
-}
-
-static int to_day_number(const mylib_date * date)
-{
-    int a = (14-date->month)/12;
-    int y = date->year + 4800 - a;
-    int m = date->month + 12*a - 3;
-    int d = date->day + ((153*m+2)/5) + 365*y
-        + (y/4) - (y/100) + (y/400) - 32045;
-    return d;
-}
-
-static mylib_date to_ymd(int day_number)
-{
-    int a = day_number + 32044;
-    int b = (4*a + 3)/146097;
-    int c = a-((146097*b)/4);
-    int d = (4*c + 3)/1461;
-    int e = c - (1461*d)/4;
-    int m = (5*e + 2)/153;
-    mylib_date date;
-    date.year = 100*b + d - 4800 + (m/10);
-    date.month = m + 3 - 12 * (m/10);
-    date.day = e - ((153*m + 2)/5) + 1;
-    return date;
-}
+#include "xyzlib.h"
 
 bool mylib_date_is_valid(const mylib_date * date)
 {
     return date->month >= 1 && date->month <= 12
         && date->day >= 1
-        && date->day <= days_in_month(date->year, date->month);
+        && date->day <= xyzlib_days_in_month(date->year, date->month);
 }
         
 void mylib_date_offset(mylib_date * date, int days)
 {
-    *date = to_ymd(to_day_number(date) + days);
+    int day_number = xyzlib_to_day_number(date->year,date->month,date->day);
+    int new_day_number = day_number + days;
+    xyzlib_to_ymd(new_day_number, &date->year, &date->month, &date->day);
 }
 
 int mylib_date_diff(const mylib_date * date, const mylib_date * other_date)
 {
-    return to_day_number(date) - to_day_number(other_date);
+    int day_number = xyzlib_to_day_number(date->year, date->month, date->day);
+    int other_day_number = xyzlib_to_day_number(
+        other_date->year, other_date->month, other_date->day);
+    return day_number - other_day_number;
 }
 
 bool mylib_date_is_equal(const mylib_date * date, const mylib_date * other_date)
--- cppf/ex2/date3.cpp
+++ cppf/ex2/date3.cpp
@@ -1,53 +1,12 @@
 #include "date3.hpp"
+#include "xyzlib.h"
 #include <stdexcept>
-
-namespace {
-    bool is_leap_year(int year)
-    {
-        return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
-    }
-
-    int days_in_month(int year, int month)
-    {
-        switch (month) {
-        case 2:
-            return is_leap_year(year) ? 29 : 28;
-        case 4: case 6: case 9: case 11:
-            return 30;
-        }
-        return 31;
-    }
-
-    int to_day_number(const mylib::date & date)
-    {
-        int a = (14-date.month())/12;
-        int y = date.year() + 4800 - a;
-        int m = date.month() + 12*a - 3;
-        int d = date.day() + ((153*m+2)/5) + 365*y
-            + (y/4) - (y/100) + (y/400) - 32045;
-        return d;
-    }
-
-    mylib::date to_date(int day_number)
-    {
-        int a = day_number + 32044;
-        int b = (4*a + 3)/146097;
-        int c = a-((146097*b)/4);
-        int d = (4*c + 3)/1461;
-        int e = c - (1461*d)/4;
-        int m = (5*e + 2)/153;
-        int year = 100*b + d - 4800 + (m/10);
-        int month = m + 3 - 12 * (m/10);
-        int day = e - ((153*m + 2)/5) + 1;
-        return mylib::date(year,month,day);
-    }
-}
 
 mylib::date::date(int year, int month, int day) :
     year_(year), month_(month), day_(day)
 {
     if (!(month >= 1 && month <= 12
-          && day >= 1 && day <= days_in_month(year, month)))
+          && day >= 1 && day <= xyzlib_days_in_month(year, month)))
         throw std::out_of_range("invalid date");
 }
 
@@ -65,13 +24,18 @@ int mylib::date::day() const {
 
 mylib::date & mylib::date::operator+=(int days)
 {
-    *this = to_date(to_day_number(*this) + days);
+    int day_number = xyzlib_to_day_number(year_, month_, day_);
+    int new_day_number = day_number + days;
+    xyzlib_to_ymd(new_day_number, &year_, &month_, &day_);
     return *this;
 }
 
 int mylib::date::operator-(const mylib::date & other) const
 {
-    return to_day_number(*this) - to_day_number(other);
+    int day_number = xyzlib_to_day_number(year_, month_, day_);
+    int other_day_number = xyzlib_to_day_number(
+        other.year_, other.month_, other.day_);
+    return day_number - other_day_number;
 }
 
 bool mylib::date::operator==(const mylib::date & other) const
