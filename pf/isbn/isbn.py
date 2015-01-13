ee0ab8b2dd19ac17c4b1aa670b39789a74b46d05
Johan Herland <johan@herland.net>
2015-01-13 11:13:53 +0100

    Minor cleanup

--- pf/isbn/isbn.py
+++ pf/isbn/isbn.py
@@ -4,12 +4,10 @@
 
 
 def _is_valid_isbn13(str):
-    if len(str) != 13:
-        return False
-    if not str.isdigit():
+    if len(str) != 13 or not str.isdigit():
         return False
     *digits, checkdigit = map(int, str)
-    res = 10 - sum(a*b for a, b in zip(digits, [1, 3]*6)) % 10
+    res = 10 - sum(a * b for a, b in zip(digits, [1, 3] * 6)) % 10
     return checkdigit == res % 10
 
 
@@ -20,7 +18,7 @@ def _is_valid_isbn10(str):
     checkchar = str[-1]
     if not chars.isdigit() or checkchar not in set('0123456789X'):
         return False
-    res = sum(a*int(b) for a, b in enumerate(chars, 1)) % 11
+    res = sum(a * int(b) for a, b in enumerate(chars, 1)) % 11
     if checkchar == 'X':
         return res == 10
     return int(checkchar) == res
